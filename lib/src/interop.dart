7e3874dc3abf9fd722de4d9b6acfc93829f8cb99
Qiong Wu <qiong.wu@gfnork.de>
2020-06-15 01:23:41 +0200

    add screen_name to options

--- lib/src/interop.dart
+++ lib/src/interop.dart
@@ -14,6 +14,7 @@ class Options {
   external factory Options(
       {String event_category,
       String event_label,
+      String screen_name,
       int value,
       String method,
       String description,
